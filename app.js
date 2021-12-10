6f4a21348f4ddd1e803cdf70ae65710a92fc07cc
sagaruttam <sagar@paktolus.com>
2021-12-10 14:39:56 +0530

    Learning

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('your code goes here!!!!!');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 10.5;
+var result = add(number1, number2);
+console.log('Result---', result);
